2f0a5def858daf2215f429928a1073db1e3ff60e
BarbaraVV <vanoli.barbara@virgilio.it>
2020-01-14 22:45:29 +0100

    03 A crowded soace

--- sketch.js
+++ sketch.js
@@ -1,68 +1,86 @@
-var spiralUno;
-var spiralDue;
+var NUM_SHAPES = 6; // total number of shapes
+var shapes = new Array(NUM_SHAPES); // array of shapes
 
-function preload() {
-
-}
+var FPS = 50; // var for the frameRate
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background('black');
+  frameRate(FPS);
+  ellipseMode(CENTER);
+  strokeWeight(3);
 
-  spiralUno = new Spiral(0, 0, 10);
-  spiralDue = new Spiral(0, 0, 5);
+  var CenterX = width / 2; // x position of the shapes
+  var CenterY = height / 2; // y position of the shapes
 
-  spiralDue.color = 'SpringGreen';
-  spiralDue.start = 22;
-
+  // instances of the object
+  shapes[0] = new Shape(CenterX, CenterY, width / 2.5, height / 3, color('#F6BCF6'));
+  shapes[1] = new Shape(CenterX, CenterY, width / 3, height / 3, color('#ED78ED'));
+  shapes[2] = new Shape(CenterX, CenterY, width / 4, height / 3, color('#A862EA'));
+  shapes[3] = new Shape(CenterX, CenterY, width / 6, height / 4, color('#D6ADEB'));
+  shapes[4] = new Shape(CenterX, CenterY, width / 9, height / 5, color('#80DFFF'));
+  shapes[5] = new Shape(CenterX, CenterY, width / 17, height / 6, color('#CCFFE6'));
 }
 
 function draw() {
+  background("black");
 
-  spiralUno.move();
-  spiralUno.display();
-  spiralDue.move();
-  spiralDue.display2();
-
-  for (var i = 0; i < 100; i++) {
-    spiralTre = new Spiral(0, 0, 60);
+  // display all the objects in the array
+  for (var i = 0; i < shapes.length; i++) {
+    shapes[i].display();
   }
-
 }
 
-function Spiral(_x, _y, _diameter) {
+// function of the object
+function Shape(_x, _y, _minSize, _maxSize, _c) {
+  this.segments = 200; // number of segments that create the shape
+  this.num_angles = TWO_PI / this.segments; // number of angles of the shape
+  this.noise_scale = 0.5; // quantity of noise
+  this.time_scale = 0.02; // time for the noise
+  this.time_diff = 1500; // difference in time
+
+  this.dx = 0;
+  this.dy = 0;
+
+  this.time_unique = random(this.time_diff); // different time of each object
   this.x = _x;
   this.y = _y;
-  this.size = _diameter;
-  this.color = 'MediumBlue';
+  this.min_rad = _minSize;
+  this.max_rad = _maxSize;
+  this.color = _c; // color of the stroke
 
-  this.start = 10;
-  this.angle = 90;
-  this.speed = 0.1;
-  this.distance = this.speed * 4;
-
-  this.move = function() {
-    this.x = cos(this.angle) * this.start;
-    this.y = sin(this.angle) * this.start;
-    this.angle += this.speed;
-    this.start = this.start + this.distance;
-
-  }
-
+  // function to display the objects
   this.display = function() {
-    translate(width / 2, height / 2);
-    fill(this.color);
-    noStroke();
-    ellipse(this.x, this.y, this.size);
+    translate(this.x, this.y);
+    stroke(this.color);
 
-  }
+    this.nextCoords(0);
+    var px = this.dx,
+      py = this.dy;
+    var i = 0;
 
-  this.display2 = function() {
-    translate(0, 0);
-    fill(this.color);
-    noStroke();
-    ellipse(this.x, this.y, this.size);
+    while (i++ != this.segments) {
+      this.nextCoords(i);
 
-  }
+      line(px, py, px = this.dx, py = this.dy);
+    }
 
+    resetMatrix();
+  };
+
+  // function to create the noise
+  this.nextCoords = function(seg) {
+    var angle = this.num_angles * seg;
+    var cosAngle = cos(angle);
+    var sinAngle = sin(angle);
+    var time = this.time_scale * frameCount + this.time_unique;
+
+    var noiseValue = noise(
+      this.noise_scale * cosAngle * this.noise_scale,
+      this.noise_scale * sinAngle * this.noise_scale, time);
+
+    var rad = this.max_rad * noiseValue - this.min_rad;
+
+    this.dx = rad * cosAngle;
+    this.dy = rad * sinAngle;
+  };
 }
